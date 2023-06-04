ede9e8702eb6488abbef3bd76928309b0d90826d
Lin-Jia-Qi <66088477+Lin-Jia-Qi@users.noreply.github.com>
2023-06-04 21:59:18 +0800

    Add files via upload

---
+++ js/all.js
@@ -0,0 +1,11 @@
+$(function(){
+    $("#ScrollToTop").click(function(){
+        $("html, body").animate(
+            {
+            scrollTop: 0
+            },
+            500
+        );
+    });    
+})
+
